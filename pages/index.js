784265b8963facc6b087aef3178b4ac26f15bcd0
Isaias Ponce <isaiasponce-12@hotmail.es>
2021-11-15 22:53:32 -0500

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -50,7 +50,7 @@ export default function Home() {
              }
            });
           console.log(repositorios);
-          setRepositories(repositorios);
+          setRepositories(repositorios);//
        
 
         }
